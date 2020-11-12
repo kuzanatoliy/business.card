accc1d61fd729352d051e7407df831695c4adee5
kuzanatoliy <njkznu@gmail.com>
2020-11-12 23:19:54 +0300

    BC: add summary page

---
+++ src/common/components/CommonList/CommonList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+export function CommonList(props) {
+  const { variant, title, itemList } = props;
+
+  const createListItem = item => (
+    <ListItem key={ item }>
+      <ListItemText>{ item }</ListItemText>
+    </ListItem>
+  );
+
+  return (
+    <>
+      <Typography variant={ variant }>{ title }</Typography>
+      <List>
+        { itemList.map(item => createListItem(item)) }
+      </List>
+    </>
+  );
+}
+
+CommonList.defaultProps = {
+  title: '',
+  variant: 'h6',
+  itemList: []
+}
--- src/common/components/index.js
+++ src/common/components/index.js
@@ -1,2 +1,3 @@
+export * from './CommonList';
 export * from './Footer';
 export * from './Header';
